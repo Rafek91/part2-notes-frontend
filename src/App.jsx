adf6216c97ccd63fc692c88cc8553f3b684b20ec
Radek Chlud <chlud@muzo.com>
2024-02-21 00:04:34 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes,setNotes] = useState([])
+  const [newNote,setNewNote] = useState('')
+  const [showAll,setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const notesObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes([...notes,notesObject])
+    setNewNote('')
+  }
+
+  const handleInputChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +29,14 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input  
+        value={newNote}
+        onChange={handleInputChange}
+        />
+        <button type='submit'>Add note</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>{showAll ? 'Show all' : 'Show filtered'}</button>
     </div>
   )
 }

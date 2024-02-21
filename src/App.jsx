8b750f4e1d60b983ee68db994a86c012731460a0
Rafek91 <radek.chlud@gmail.com>
2024-02-21 15:53:44 +0100

    adding filtering

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,13 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll ? notes : notes.filter( note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
